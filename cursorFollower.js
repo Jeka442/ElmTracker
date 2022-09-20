4b24d44974f5bfb1d6c3c9cc69ff9d18bf34c470
Jeka <jeka.gudilov@allgreen.app>
2022-09-21 00:36:37 +0300

    must sleep!

--- cursorFollower.js
+++ cursorFollower.js
@@ -8,18 +8,22 @@ div.style = `position:fixed; background-color:black; color:white; z-index:214748
 
 document.body.addEventListener("mousemove", (e) => {
     const elm = document.getElementById("ElmTracker-ActiveSpan");
+    let isVisible = true;
     if (!elm) {
-        div.style.opacity = 0;
+        isVisible = false;
     } else {
         if (elm.getAttribute("isActive") == "active") {
-            div.style.opacity = 1;
+            isVisible = true;
         } else {
-            div.style.opacity = 0;
+            isVisible = false;
         }
     }
-    div.innerText = `<${e.target.tagName.toLowerCase()}>`
-    let clientW = document.body.clientWidth;
-    let offsetFollowerX = (clientW / 2) < e.clientX ? -50 : 20;
-    div.style.left = e.clientX + offsetFollowerX + 'px';
-    div.style.top = e.clientY + 25 + 'px';
+    div.style.opacity = isVisible ? 1 : 0;
+    if (isVisible) {
+        div.innerText = `<${e.target.tagName.toLowerCase()}>`
+        let clientW = document.body.clientWidth;
+        let offsetFollowerX = (clientW / 2) < e.clientX ? -50 : 20;
+        div.style.left = e.clientX + offsetFollowerX + 'px';
+        div.style.top = e.clientY + 25 + 'px';
+    }
 })
